e4cb8f78e57fe2a2eb4e46f301d9b160acfd48eb
StanTim <1694569@mail.ru>
2022-09-07 10:40:27 +0300

    add destroy, edit, update methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
@@ -18,8 +18,30 @@ class PostsController < ApplicationController
     end
   end
 
-  def update
+  def show
+    @post = Post.find(params[:id])
+  end
 
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.updatr(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private
